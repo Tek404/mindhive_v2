645fa3a42f2144e3132269f1ec2d52df20ab8395
Tek404 <khooeik@gmail.com>
2024-02-03 00:58:23 +0800

    fix

--- outlet_backend/geo_outlet/views.py
+++ outlet_backend/geo_outlet/views.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers, generics
 from .models import Outlet
-from transformers import Pipeline
+from transformers import pipeline
 
 class OutletSerializer(serializers.ModelSerializer):
     class Meta:
